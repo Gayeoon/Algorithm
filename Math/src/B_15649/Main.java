e9d2dcc117482d775832372121b888ad32b81227
Gayeoon <rkdus1177@naver.com>
2020-07-20 15:54:51 +0900

    백준 15649 N과 M [Math - Permutation] 문제해결

---
+++ Math/src/B_15649/Main.java
@@ -0,0 +1,37 @@
+package B_15649;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		int m = input.nextInt();
+		
+		LinkedList<Integer> list = new LinkedList<>();
+		boolean check[] = new boolean[n+1];
+		permutation(n, m, list, check);
+	}
+	
+	static void permutation(int n, int m, LinkedList<Integer> list, boolean[] check) {
+		if(list.size() == m) {
+			for(int i : list)
+				System.out.print(i+" ");
+			System.out.println();
+		}else {
+			for(int i=1; i<=n; i++) {
+				if(check[i]) continue;
+				check[i] = true;
+				list.add(i);
+				permutation(n, m, list, check);
+				list.removeLast();
+				check[i] = false;
+			}
+		}
+	}
+
+}
