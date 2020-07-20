a13fe1604de58fc7174b0414ca9d5070ff446f04
Gayeoon <rkdus1177@naver.com>
2020-07-20 16:08:00 +0900

    백준 15650 N과 M(2) [Math - Permutation] 문제해결

--- Math/src/B_15649/Main.java
+++ Math/src/B_15649/Main.java
@@ -4,32 +4,40 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
+	static LinkedList<Integer> ck = new LinkedList<>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		
+
 		int n = input.nextInt();
 		int m = input.nextInt();
-		
+
 		LinkedList<Integer> list = new LinkedList<>();
-		boolean check[] = new boolean[n+1];
+		boolean check[] = new boolean[n + 1];
 		permutation(n, m, list, check);
 	}
-	
+
 	static void permutation(int n, int m, LinkedList<Integer> list, boolean[] check) {
-		if(list.size() == m) {
-			for(int i : list)
-				System.out.print(i+" ");
+		if (list.size() == m) {
+			for (int i : list)
+				System.out.print(i + " ");
 			System.out.println();
-		}else {
-			for(int i=1; i<=n; i++) {
-				if(check[i]) continue;
+		} else {
+			int cnt = 0;
+			for (int i = 1; i <= n; i++) {
+				if (check[i] || ck.contains(i))
+					continue;
 				check[i] = true;
 				list.add(i);
 				permutation(n, m, list, check);
 				list.removeLast();
 				check[i] = false;
+				ck.add(i);
+				cnt++;
+			}
+			for(int j=cnt; j > 0; j--) {
+				ck.removeLast();
 			}
 		}
 	}
