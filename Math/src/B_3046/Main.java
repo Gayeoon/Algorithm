49847c1282b3ea0994941ce0e174c2366ddd70e0
Gayeoon <rkdus1177@naver.com>
2020-07-21 15:24:59 +0900

    백준 3046 R2 [Math] 문제해결

---
+++ Math/src/B_3046/Main.java
@@ -0,0 +1,16 @@
+package B_3046;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int r = input.nextInt();
+		int s = input.nextInt();
+		
+		System.out.println(2*s - r);
+	}
+
+}
