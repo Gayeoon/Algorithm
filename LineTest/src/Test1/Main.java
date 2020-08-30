4884b94f86a633a57fb8b293b7d15ea1dc9df377
Gayeoon <rkdus1177@naver.com>
2020-08-30 23:48:13 +0900

    2020 Line 코딩테스트 [Problem #1]

---
+++ LineTest/src/Test1/Main.java
@@ -0,0 +1,79 @@
+package Test1;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String inputString = "if (Count of eggs is 4.) {Buy milk.}";
+
+		int answer = 0;
+		List<Character> list = new ArrayList<>();
+		for (int i = 0; i < inputString.length(); i++) {
+			boolean flag = false;
+			char temp = inputString.charAt(i);
+			if (temp == ')') {
+				for (int l = 0; l < list.size(); l++) {
+					if (list.get(l) == '(') {
+						list.remove(l);
+						answer++;
+						flag = true;
+						break;
+					}
+				}
+				if (!flag) {
+					// return -1;
+					System.out.println(-1);
+				}
+			} else if (temp == '}') {
+				for (int l = 0; l < list.size(); l++) {
+					if (list.get(l) == '{') {
+						list.remove(l);
+						answer++;
+						flag = true;
+						break;
+					}
+				}
+				if (!flag) {
+					// return -1;
+					System.out.println(-1);
+				}
+			} else if (temp == ']') {
+				for (int l = 0; l < list.size(); l++) {
+					if (list.get(l) == '[') {
+						list.remove(l);
+						answer++;
+						flag = true;
+						break;
+					}
+				}
+				if (!flag) {
+					// return -1;
+					System.out.println(-1);
+				}
+			} else if (temp == '>') {
+				for (int l = 0; l < list.size(); l++) {
+					if (list.get(l) == '<') {
+						list.remove(l);
+						answer++;
+						flag = true;
+						break;
+					}
+				}
+				if (!flag) {
+					// return -1;
+					System.out.println(-1);
+				}
+			} else if (temp == '(' || temp == '[' || temp == '{' || temp == '<')
+				list.add(inputString.charAt(i));
+		}
+		if (list.size() == 0) {
+			// return answer;
+			System.out.println(answer);
+		} else {
+			// return -1;
+			System.out.println(-1);
+		}
+	}
+}
