a4c4188fb622c920cebf2fad15fc7132adef5838
Gayeoon <rkdus1177@naver.com>
2020-08-30 23:51:52 +0900

    2020 Line 코딩테스트 [Problem #3]

--- LineTest/src/Test3/Main.java
+++ LineTest/src/Test3/Main.java
@@ -1,36 +1,56 @@
 package Test3;
 
+class Road {
+	int nomal, idx;
+
+	Road(int nomal, int idx) {
+		this.nomal = nomal;
+		this.idx = idx;
+	}
+}
+
 public class Main {
+
+	static Road[] damage;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String answer_sheet = "4132315142";
-		String[] sheets = { "3241523133", "4121314445", "3243523133", "4433325251", "2412313253" };
-		int answer = 0;
-		for (int i = 0; i < sheets.length; i++) {
-			int total = 0;
-			int conti = 0;
-			int max = 0;
-			int prev = -1;
-			for (int j = 0; j < answer_sheet.length(); j++) {
-				boolean flag = false;
-				if (answer_sheet.charAt(j) != sheets[i].charAt(j)) {
-					flag = true;
-					total++;
-				}
-				if (flag) {
-					if (prev + 1 == j) {
-						conti++;
-						prev = j;
-					}
-				}else {
-					conti = 0;
-				}
-				max = Math.max(max, conti);
-			}
-			answer = Math.max(answer, (int) (total + Math.pow(max, 2)));
-		}
 
+		String road = "001100";
+		int n = 2;
+		int answer = -1;
+		damage = new Road[road.length() + 1];
+		int cnt = 0;
+		int idx = 0;
+		for (int i = 0; i < road.length(); i++) {
+			if (road.charAt(i) == '0') {
+				damage[idx] = new Road(cnt, i);
+				cnt = 0;
+				idx++;
+			} else {
+				cnt++;
+			}
+		}
+		if(idx <= n)
+			System.out.println(road.length());
+		
+		damage[idx] = new Road(cnt, road.length());
+		
+		idx++;
+		answer = solve(0, idx, n);
 		System.out.println(answer);
+	}
+
+	static int solve(int start, int idx, int n) {
+		if (start >= idx - n) {
+			return 0;
+		}
+		int count = 0;
+		for (int i = start; i < start + n + 1; i++) {
+			count += damage[i].nomal + 1;
+		}
+		count--;
+		count = Math.max(count, solve(start + 1, idx, n));
+		return count;
 	}
 }
