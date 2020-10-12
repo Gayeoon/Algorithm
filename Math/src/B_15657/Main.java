3e82d3aa4d77e13e83e273702a7b3f045dcc1646
Gayeoon <rkdus1177@naver.com>
2020-10-12 19:43:33 +0900

    백준 15657 N과 M(8) [Math] 문제해결

---
+++ Math/src/B_15657/Main.java
@@ -0,0 +1,36 @@
+package B_15657;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int arr[];
+	static StringBuffer sb = new StringBuffer();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int m = input.nextInt();
+
+		arr = new int[n];
+
+		for (int i = 0; i < n; i++)
+			arr[i] = input.nextInt();
+
+		Arrays.sort(arr);
+
+		solve(0, 0, n, m, "");
+		System.out.print(sb);
+	}
+
+	static void solve(int depth, int idx, int n, int m, String str) {
+		if (depth >= m) {
+			sb.append(str + "\n");
+			return;
+		}
+		for (int i = idx; i < n; i++) {
+			solve(depth + 1, i, n, m, str + arr[i] + " ");
+		}
+	}
+}
